6449d348b1b9396dd755edbe9324536c0b7d88b9
davidchin <davidchin.yf@gmail.com>
2014-04-26 16:39:05 +1000

    Add some config files

---
+++ Gruntfile.js
@@ -0,0 +1,84 @@
+module.exports = function (grunt) {
+  'use strict';
+
+  grunt.initConfig({
+    // Configure tasks
+    pkg: grunt.file.readJSON('bower.json'),
+
+    banner: '/*!\n' +
+            ' * <%= pkg.name %>.js v<%= pkg.version %>\n' +
+            ' * <%= pkg.homepage %>\n' +
+            ' * Copyright <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>\n' +
+            ' * <%= pkg.license %> License\n' +
+            ' */\n',
+
+    jshint: {
+      all: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js']
+    },
+
+    concat: {
+      options: {
+        banner: '<%= banner %>',
+        stripBanners: true
+      },
+
+      dist: {
+        src: ['src/module.js', 'src/**/*.js'],
+        dest: '<%= pkg.name %>.js'
+      }
+    },
+
+    ngmin: {
+      dist: {
+        src: '<%= pkg.name %>.js',
+        dest: '<%= pkg.name %>.min.js'
+      }
+    },
+
+    uglify: {
+      options: {
+        banner: '<%= banner %>'
+      },
+
+      dist: {
+        src: '<%= pkg.name %>.js',
+        dest: '<%= pkg.name %>.min.js'
+      }
+    },
+
+    clean: {
+      dist: [
+        '<%= pkg.name %>.js',
+        '<%= pkg.name %>.min.js'
+      ]
+    },
+
+    watch: {
+      dist: {
+        files: ['src/**/*.js'],
+        tasks: ['build']
+      }
+    },
+
+    bump: {
+      options: {
+        files: ['package.json', 'bower.json'],
+        commitFiles: ['package.json', 'bower.json'],
+        pushTo: 'origin'
+      }
+    }
+  });
+
+  // Load tasks
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-bump');
+  grunt.loadNpmTasks('grunt-ngmin');
+
+  // Register custom tasks
+  grunt.registerTask('test', ['karma']);
+  grunt.registerTask('build', ['jshint', 'clean', 'concat', 'ngmin', 'uglify']);
+};
